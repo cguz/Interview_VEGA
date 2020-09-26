106a186f006345a1ee07c4648f997ce22c131ca1
Cesar Guzman <developer@cguz.org>
2020-09-26 19:43:07 +0200

    Implementing the basic algorithm
    Read the file pass schedule and store the classes.
    
    To do:
    
    [ ] Implement the basic algorithm
    [ ] Approach 2

--- code/src/code/service/GetWYSpaceFactory.java
+++ code/src/code/service/GetWYSpaceFactory.java
@@ -3,16 +3,37 @@ package code.service;
 import code.common.enumerate.WYSPACE;
 import code.common.WYSpaceI;
 
+/**
+ * 
+ * @author Cesar Augusto Guzman Alvarez
+ * @date Sep 26, 2020
+ *
+ */
 public class GetWYSpaceFactory {
 
+	/**
+	 * Function to retrieve the approach used to solve the Space
+	 * 
+	 * @param approach Enumerate of the approach to select
+	 * @return WYSpaceI that solve the problem
+	 */
 	public WYSpaceI getSpace(WYSPACE approach) {
 		
 		switch(approach) {
-		
+			/**
+			 * Visit each time and test the satellites that intersect 
+			 * the given time within its interval of communication, 
+			 * which requires O(n*m). Where n is the number of 
+			 * time intervals during a day, and m is the number of satellites. 
+			 */
 			case APPROACH1_ALMOST_CUADRATIC:
 				return new WYSpaceApproach1();
+			/**
+			 * Interval tree
+			 */
 			case APPROACH2_INTERVAL_TREE:
 				return new WYSpaceApproach2();
+				
 		}
 		
 		return null;
--- code/src/code/service/WYSpaceAbstract.java
+++ code/src/code/service/WYSpaceAbstract.java
@@ -3,11 +3,13 @@ package code.service;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import java.time.LocalTime;
 
 import code.common.Interval;
+import code.common.Satellite;
 import code.common.Station;
 import code.common.StationGround;
 import code.common.WYSpaceI;
@@ -18,6 +20,9 @@ import code.common.WYSpaceI;
  *
  */
 public abstract class WYSpaceAbstract implements WYSpaceI {
+	
+	// delimiter of the file
+	private final String DELIMITER = ",";
 	
 	/**
 	 * Pass schedule configuration file
@@ -33,8 +38,10 @@ public abstract class WYSpaceAbstract implements WYSpaceI {
 	 * List of satellites
 	 */
 	private List<Station> satellites;
-	
 
+	
+	// LocalTime.parse("00:00");
+    // System.out.println(periodTotalDownlinkMaximum);
 	@Override
 	public LocalTime findPeriod(String file, Integer maxBandwidth) {
 
@@ -42,25 +49,72 @@ public abstract class WYSpaceAbstract implements WYSpaceI {
 		
 		stationGround = new StationGround(maxBandwidth);
 		
-		LocalTime periodTotalDownlinkMaximum = null;
-		// LocalTime.parse("00:00");
-        // System.out.println(periodTotalDownlinkMaximum);
+		// read the file pass schedule
+		readFile(fileName);
 		
+		
+		// declare a list of intervals as empty
+		List<LocalTime> periodTotalDownlinkMaximum = new ArrayList<LocalTime>();
+
+		// maximum total band
+		// maxBand = 0
+
+		// for each possible time in the interval
+		// for time 00:00 to 00:00 (24:00) increased by 30:
+		    
+		    // calculate the total bandwidth occupied by the satellites in the given time
+		    // totalBandwidth = 0
+			// for each satellite in pass schedule:
+				// if satellite.inPass(time) :
+		            // totalBandwidth+= satellite.getBandwidth()
+
+		    // whether the total bandwidth is higher than the support by the ground station
+		    // if totalBandwidth <= stationGround.getBandwidth():
+
+		        // keep the maximum total bandwidth 
+		        // if totalBandwidth > maxBand :
+					// periodTotalDownlinkMaximum.clear() 
+		        // if totalBandwidth >= maxBand :
+		            // maxBand = totalBandwidth
+		            // periodTotalDownlinkMaximum.add(time);
+		
+		return null; // periodTotalDownlinkMaximum;
+		
+	}
+
+
+	/**
+	 * Function to read the file pass schedule
+	 * 
+	 * @param fileName String name of the file pass schedule
+	 */
+	private void readFile(String fileName) {
+
 		try {
+			// read the file
 			File input = new File(fileName);
-			
 			Scanner scanner = new Scanner(input);
 			
+			// whether there is more line in the file
 			while(scanner.hasNextLine()) {
+				
+				// read the next line of the file
 				String line = scanner.nextLine();
-				System.out.println(line);
+				
+				// separate the string 
+				String[] temp = line.split(DELIMITER);
+				
+				// create the object Satellite
+				Satellite satellite = new Satellite(temp[0], Integer.parseInt(temp[1]));
+				
+				// keep the satellite information in the list
+				satellite.add(new Interval(LocalTime.parse(temp[2]), LocalTime.parse(temp[3])));
+				
 			}
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		return periodTotalDownlinkMaximum;
 	}
 	
 }
